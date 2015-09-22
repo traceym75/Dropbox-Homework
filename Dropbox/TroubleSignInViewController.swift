a406776ef65e6e24f0990fdf97867ce669c9d6f9
Tracey Ewart <traceym75@gmail.com>
2015-09-21 23:12:13 -0700

    commit

---
+++ Dropbox/TroubleSignInViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TroubleSignInViewController.swift
+//  Dropbox
+//
+//  Created by Tracey Ewart on 9/19/15.
+//  Copyright © 2015 Tracey Ewart. All rights reserved.
+//
+
+import UIKit
+
+class TroubleSignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func onCancelButton(sender: AnyObject) {
+        
+        navigationController!.popViewControllerAnimated(true)
+    }
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
